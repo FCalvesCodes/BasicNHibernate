6696152aa86d9037628c0614d56132d4bc5705fc
Felipe Calves <37254727+FelipeAlmeid4@users.noreply.github.com>
2022-02-25 15:08:14 -0400

    Update Program.cs

--- BasicNHibernate.Console/Program.cs
+++ BasicNHibernate.Console/Program.cs
@@ -1,7 +1,7 @@
 ﻿using BasicNHibernate.Console.Data;
 using BasicNHibernate.Console.Domain.Users;
 
-var connString = "Server=localhost; Port=5432; User Id=postgres; Password=cs305; Database=academicschedule";
+var connString = "Server=localhost; Port=5432; User Id=postgres; Password=123; Database=academicschedule";
 
 using (var context = new NHibernateManager(connString))
 {
